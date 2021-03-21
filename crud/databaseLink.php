5241862466f9c512f04207d207ec6f9fbb07aba6
okoli-ryan50 <mariaunawu@gmail.com>
2021-03-21 02:22:36 +0100

    crud app

--- crud/databaseLink.php
+++ crud/databaseLink.php
@@ -1,9 +1,12 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'Ugo');
-define('DB_PASSWORD', 'okoliryan50');
-define('DB_NAME', 'sideHustle');
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$DB_SERVER = $cleardb_url["host"];
+$DB_USERNAME = $cleardb_url["user"];
+$DB_PASSWORD = $cleardb_url["pass"];
+$DB_NAME = substr($cleardb_url["path"], 1);
+
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
